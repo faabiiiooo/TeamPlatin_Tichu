e3d55351ac78e518d5c1dadabf2d4022818da612
pascal.marbach@hotmail.com <(Breakdance1996)>
2019-10-31 10:23:28 +0100

    31.10.19: Klasse Team und Seat Commitet

---
+++ src/server/model/Srv_Seat.java
@@ -0,0 +1,11 @@
+package server.model;
+
+public class Srv_Seat {
+
+    private final int SEAT_ID=0;
+    private Player player;
+
+    public Srv_Seat(){
+
+    }
+}
---
+++ src/server/model/Srv_Team.java
@@ -0,0 +1,43 @@
+package server.model;
+
+import java.util.ArrayList;
+
+public class Srv_Team {
+
+    private final int TEAM_ID = 0;
+    private final int MAX_TEAM_MEMBERS=2;
+    private ArrayList<Player>members;
+    private int gameScore;
+    private int roundScore;
+
+
+
+
+
+
+    public Srv_Team(){
+        this.members=new ArrayList<Player>();
+
+
+    }
+
+    private Seat bookSeat(){
+
+        return Seat;
+
+
+    }
+
+    public void calcGameScore(){
+
+    }
+
+    public void resetRoundScore(){
+
+    }
+
+    public void calcRoundScore(){
+
+    }
+
+}
