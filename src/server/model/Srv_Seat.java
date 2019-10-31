80dcaf005b53e2e8306c1a803d07ce06b8954e0f
pascal.marbach@hotmail.com <(Breakdance1996)>
2019-10-31 18:06:35 +0100

    31.10.19: Team

--- src/server/model/Srv_Seat.java
+++ src/server/model/Srv_Seat.java
@@ -5,7 +5,12 @@ public class Srv_Seat {
     private final int SEAT_ID=0;
     private Player player;
 
-    public Srv_Seat(){
+    public Srv_Seat(int SEAT_ID){
+
+
+
+       }
+
 
     }
 }
--- src/server/model/Srv_Team.java
+++ src/server/model/Srv_Team.java
@@ -6,7 +6,7 @@ public class Srv_Team {
 
     private final int TEAM_ID = 0;
     private final int MAX_TEAM_MEMBERS=2;
-    private ArrayList<Player>members;
+    private ArrayList<Srv_Player>members;
     private int gameScore;
     private int roundScore;
 
@@ -16,14 +16,17 @@ public class Srv_Team {
 
 
     public Srv_Team(){
-        this.members=new ArrayList<Player>();
+        this.members=new ArrayList<Srv_Player>();
+
+
+
 
 
     }
 
-    private Seat bookSeat(){
-
-        return Seat;
+    private Srv_Seat bookSeat(){
+        Srv_Seat seat=new Srv_Seat(int SEAT_ID);
+        return seat;
 
 
     }
