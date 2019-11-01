13908652ed8ad1c18ec90cab0ab55605a2f3e9c4
sandr094 <sandro.zimmermann@students.fhnw.ch>
2019-11-01 16:07:55 +0100

    Implementierung HandType V2 SZ

--- src/server/model/Srv_HandType.java
+++ src/server/model/Srv_HandType.java
@@ -1,27 +1,77 @@
 package server.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public enum Srv_HandType {
     HighCard, OnePair, XPair, Tripple, Street, FullHouse, Bomb;
 
-    public static Srv_HandType evaluateHand(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
-        Srv_HandType currentEval = HighCard;
+    public static boolean evaluateHand(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
+        boolean isHigher = false;
 
-        if (isOnePair(tableCards, playerCards, rank)) currentEval = OnePair;
+        if (tableCards.size() == 0 || isHighCard(tableCards) && isHighCard(playerCards) && playerCards.size() == 1) { //Case HighCard (Table&Player have 1 card or no cards on the table)
 
-        return currentEval;
+        }
+        if (tableCards.size() == 0 || isOnePair(tableCards) && isOnePair(playerCards) && playerCards.size() == 2) { //Case OnePair (Table&Player have 2 cards or no cards on the table)
+            Collections.sort(tableCards);
+            Collections.sort(playerCards);
+            if (tableCards.get(1).getRank().ordinal()< playerCards.get(1).getRank().ordinal()) { //Which second card is higher? Maybe first Card is a Phoenix
+                isHigher = true;
+            }
+        }
+
+        return isHigher;
     }
 
-    public static boolean isHigher(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
-
+    public static boolean isHighCard(ArrayList<Srv_Card> cards) {
+        return false;
     }
 
-    public static boolean isOnePair(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
-
+    public static boolean isOnePair(ArrayList<Srv_Card> cards) {
+        boolean found = false;
+        Collections.sort(cards);
+        if (includesSpecialCards(cards) == false) { //No special card included? -> regular process
+            for (int i = 0; i < cards.size() - 1 && !found; i++) {
+                for (int j = i+1; j < cards.size() && !found; j++) {
+                    if (cards.get(i).getRank() == cards.get(j).getRank()) {
+                        found = true;
+                    }
+                }
+            }
+        } else { // special card included?
+            if (cards.get(0).getRank() == Srv_Rank.Phoenix) { //Is it a phoenix? Case 2 specialCard: Phoenix has not the highest rank
+                found = true;
+            }
+        }
+        return found;
     }
 
     public static boolean isXPair(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
+        return false;
+    }
 
+    public static boolean isTripple(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
+        return false;
+    }
+
+    public static boolean isStreet(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
+        return false;
+    }
+
+    public static boolean isFullHouse(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
+        return false;
+    }
+
+    public static boolean isBomb(ArrayList<Srv_Card> tableCards, ArrayList<Srv_Card> playerCards, Srv_Rank rank) {
+        return false;
+    }
+
+    public static boolean includesSpecialCards(ArrayList<Srv_Card> cards) {
+        boolean found = false;
+        Collections.sort(cards);
+        if (cards.get(0).getSuit() == Srv_Suit.SpecialCards) { //SpecialCards would be always the highest card
+            found = true;
+        }
+        return found;
     }
 }
--- src/server/model/Srv_HandTypeTest.java
+++ src/server/model/Srv_HandTypeTest.java
@@ -5,26 +5,100 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class Srv_HandTypeTest {
 
     private static String[][] highCards = {
-            { "2S", "9C", "3H", "5D", "7H" },
+            { "2S", "9P", "3K", "5K", "7S" },
+    };
+
+    private static String[][] onePairCardsTable = {
+            { "5S", "5K" },
+    };
+
+    private static String[][] onePairCardsPlayer = {
+            { "6S", "PE" },
     };
 
     // This is where we store the translated hands
     ArrayList<ArrayList<Srv_Card>> highCardHands;
+    ArrayList<ArrayList<Srv_Card>> onePairHandsTable;
+    ArrayList<ArrayList<Srv_Card>> onePairHandsPlayer;
 
     /**
      * The makeHands method is called before each test method,
      * and prepares the translated hands. We recreate these for
-     * each test method, in case the test method damages the data.
+     * each test method, in case the test method damages the data. */
 
     @Before
     public void makeHands() {
         highCardHands = makeHands(highCards);
-    }      */
+        onePairHandsTable = makeHands(onePairCardsTable);
+        onePairHandsPlayer = makeHands(onePairCardsPlayer);
+    }
 
-    @Test // This is the test method for the isOnePair in HandType.
+    @Test // This is the test method for isHigher in HandType.
+    public void testEvaluateHand() {
+        for (ArrayList<Srv_Card> handTable : onePairHandsTable) {
+            for (ArrayList<Srv_Card> handPlayer : onePairHandsPlayer) {
+                assertTrue(Srv_HandType.evaluateHand(handTable, handPlayer, null));
+            }
+        }
+    }
+
+    @Test // This is the test method for isOnePair in HandType.
     public void testIsOnePair() {
+        for (ArrayList<Srv_Card> hand : onePairHandsPlayer) {
+                assertTrue(Srv_HandType.isOnePair(hand));
+        }
+    }
+
+    /**
+     * Make an ArrayList of hands from an array of string-arrays
+     */
+    private ArrayList<ArrayList<Srv_Card>> makeHands(String[][] handsIn) {
+        ArrayList<ArrayList<Srv_Card>> handsOut = new ArrayList<>();
+        for (String[] hand : handsIn) {
+            handsOut.add(makeHand(hand));
+        }
+        return handsOut;
+    }
+
+    /**
+     * Make a hand (ArrayList<Card>) from an array of 5 strings
+     */
+    private ArrayList<Srv_Card> makeHand(String[] inStrings) {
+        ArrayList<Srv_Card> hand = new ArrayList<>();
+        for (String in : inStrings) {
+            hand.add(makeCard(in));
+        }
+        return hand;
+    }
+
+    private Srv_Card makeCard(String in) {
+        char r = in.charAt(0);
+        Srv_Rank rank = null;
+        if (r <= '9') rank = Srv_Rank.values()[r-'0' - 2];
+        else if (r == 'T') rank = Srv_Rank.Ten;
+        else if (r == 'J') rank = Srv_Rank.Jack;
+        else if (r == 'Q') rank = Srv_Rank.Queen;
+        else if (r == 'K') rank = Srv_Rank.King;
+        else if (r == 'A') rank = Srv_Rank.Ace;
+        else if (r == 'P') rank = Srv_Rank.Phoenix;
+        else if (r == 'D') rank = Srv_Rank.Dragon;
+        else if (r == 'W') rank = Srv_Rank.Dog; //Dog = Wuff
+        else if (r == 'M') rank = Srv_Rank.Mahjong;
+
+        char s = in.charAt(1);
+        Srv_Suit suit = null;
+        if (s == 'J') suit = Srv_Suit.Jade;
+        if (s == 'P') suit = Srv_Suit.Pagodas;
+        if (s == 'S') suit = Srv_Suit.Stars;
+        if (s == 'K') suit = Srv_Suit.Swords; //Swords = Knife
+        if (s == 'E') suit = Srv_Suit.SpecialCards; //SpecialCards = Extra
+
+        return new Srv_Card(suit, rank, 0); //For HandType-testing: value not relevant
     }
 }
