11f1e5f06c7f09c03bdfae260ce0fe4b856e1b70
pascal.marbach@hotmail.com <(Breakdance1996)>
2019-11-03 15:58:17 +0100

    3.11.19 Send and recive Methods + run()

--- src/client/model/Clt_Client.java
+++ src/client/model/Clt_Client.java
@@ -1,9 +1,15 @@
 package client.model;
 
+import client.controller.Clt_Controller;
+import resources.Message;
+import resources.ServiceLocator;
+import server.model.Srv_ClientThread;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 
 public class Clt_Client extends Thread{
@@ -11,6 +17,9 @@ public class Clt_Client extends Thread{
     private final int PORT=10456;
     private Socket socket;
     private String serverIP;
+    private Clt_Controller controller;
+    private final ServiceLocator serviceLocator = ServiceLocator.getServiceLocator();
+    private final Logger logger = serviceLocator.getLogger();
 
 
     public Clt_Client(String serverIP,int PORT){
@@ -18,20 +27,53 @@ public class Clt_Client extends Thread{
             socket=new Socket(serverIP,PORT);
 
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.severe("Socket could not be created");
         }
 
     }
     @Override
     public void run(){
-        try (ObjectInputStream in =new ObjectInputStream(socket.getInputStream())){
-            ObjectOutputStream out=new ObjectOutputStream(socket.getOutputStream());
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        try{
+            while (true){
+                if(this.receive() != null ){
+                    Message msgIn = this.receive();
+                    logger.info("Message received from Server. Message Type: "+msgIn.getType());
+                    Message msgOut =controller.processIncomingMessage(msgIn);
+                    if(msgOut != null){
+                        this.send(msgOut);
+                    }
+                }
+
+            }
+
+        } catch (Exception e){
         }
+
 
     }
 
+    public  Message receive () throws Exception { // Recive Messages from Server /Pascal
 
-}
+
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            Message msgIn = (Message) in.readObject();
+            return msgIn;
+
+
+        }
+
+        public void send(Message msgOut){ // Send Messages to the Server /Pascal
+
+        try{
+            ObjectOutputStream out=new ObjectOutputStream(socket.getOutputStream());
+            out.writeObject(this);
+            out.flush();
+
+
+        } catch (IOException e) {
+           logger.severe("Could not send a Message");
+        }
+
+        }
+    }
+
