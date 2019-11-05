df5b92ac90ef4a6eca76149b286e0056c685799a
sandr094 <sandro.zimmermann@students.fhnw.ch>
2019-11-05 21:26:15 +0100

    Implementierung Tripple, Street V1 SZ

--- src/server/model/Srv_HandType.java
+++ src/server/model/Srv_HandType.java
@@ -49,7 +49,7 @@ public enum Srv_HandType {
             if (includesSpecialCards(cards) == false) { //No special card included? -> regular process
                 for (int i = 0; i < cards.size() - 1 && !found; i++) {
                     for (int j = i + 1; j < cards.size() && !found; j++) {
-                        if (cards.get(i).getRank() == cards.get(j).getRank()) {
+                        if (cards.get(i).getRank() == cards.get(j).getRank()) { //2 cards with same rank = onePair
                             found = true;
                         }
                     }
@@ -57,7 +57,7 @@ public enum Srv_HandType {
             } else { // special card included?
                 if (cards.get(0).getRank() == Srv_Rank.Phoenix) { //Is it a phoenix? Case 2 specialCard: Phoenix has not the highest rank
                     callSpecialCard(cards);
-                    found = true;
+                    found = true; // 1 card and phoenix as joker = onePair
                 }
             }
         }
@@ -68,19 +68,67 @@ public enum Srv_HandType {
         return false;
     }
 
-    public static boolean isTripple(ArrayList<Srv_Card> tableCards) {
+    public static boolean isTripple(ArrayList<Srv_Card> cards) { //@author Sandro
+        boolean found = false;
+        Collections.sort(cards);
+        if (cards.size() == 3) { //only 3 cards allowed
+            if (includesSpecialCards(cards) == false) { //No special card included? -> regular process
+                if (cards.get(0).getRank() == cards.get(1).getRank() && cards.get(1).getRank() == cards.get(2).getRank()) {
+                    found = true; //3 cards with same rank = Tripple
+                }
+            } else { // special card included?
+                if (cards.get(0).getRank() == Srv_Rank.Phoenix) { //Is it a phoenix? Case 2 specialCard: Phoenix has not the highest rank
+                    if (cards.get(1).getRank() == cards.get(2).getRank()) { //Other 2 cards same rank?
+                        callSpecialCard(cards);
+                        found = true; //2 cards with same rank and Phoenix as joker = Tripple
+                    }
+                }
+            }
+        }
+        return found;
+    }
+
+    public static boolean isStreet(ArrayList<Srv_Card> cards) {
+        boolean found = false;
+        int counter = 1; // Counter of the correct cards
+        Collections.sort(cards); // Sort the cards from high to low ordinal
+        logger.info("CASE isStreet");
+        logger.info("Cards: "+cards);
+
+        if (cards.size() >= 5) { //only 5 cards or more allowed
+            logger.info("5 or more cards? "+cards.size());
+            if (includesSpecialCards(cards) == false) { //No special card included? -> regular process
+                logger.info("No specialCard");
+                for(int i = 0; i < cards.size()-1; i++) {
+                    if(cards.get(i).getRank().ordinal() == cards.get(i+1).getRank().ordinal()+1) {
+                        counter++;
+                        logger.info("Counter: "+counter);
+                    }
+                    if (counter >= 5) { // -> 5 Ordinals or more in succesion = Street
+                        found = true;
+                    }
+                }
+            } else { // special card included?
+                if (cards.get(0).getRank() == Srv_Rank.Mahjong && cards.get(1).getRank() != Srv_Rank.Phoenix) { //Is it a mahjong?
+                    callSpecialCard(cards);
+                }
+                if (cards.get(0).getRank() == Srv_Rank.Phoenix) { //Is it a phoenix?
+                    callSpecialCard(cards);
+                }
+                if (cards.get(0).getRank() == Srv_Rank.Mahjong && cards.get(1).getRank() == Srv_Rank.Phoenix) { //Is it a phoenix and a mahjong?
+                    callSpecialCard(cards);
+                }
+
+            }
+        }
+        return found;
+    }
+
+    public static boolean isFullHouse(ArrayList<Srv_Card> cards) {
         return false;
     }
 
-    public static boolean isStreet(ArrayList<Srv_Card> tableCards) {
-        return false;
-    }
-
-    public static boolean isFullHouse(ArrayList<Srv_Card> tableCards) {
-        return false;
-    }
-
-    public static boolean isBomb(ArrayList<Srv_Card> tableCards) {
+    public static boolean isBomb(ArrayList<Srv_Card> cards) {
         return false;
     }
 
@@ -136,6 +184,19 @@ public enum Srv_HandType {
                         isHigher = true;
                     } else {
                         if (tableCards.size() == 2) { //Two cards are on the table
+                            Collections.sort(tableCards);
+                            Collections.sort(playerCards);
+                            if (tableCards.get(1).getRank().ordinal() < playerCards.get(1).getRank().ordinal()) { //Which second card is higher? Maybe first Card is a Phoenix
+                                isHigher = true;
+                            }
+                        }
+                    }
+                    break;
+                case Tripple:
+                    if (tableCards.size() == 0) { //No card on the table -> Player has automatically the higher Tripple
+                        isHigher = true;
+                    } else {
+                        if (tableCards.size() == 3) { //Three cards are on the table
                             Collections.sort(tableCards);
                             Collections.sort(playerCards);
                             if (tableCards.get(1).getRank().ordinal() < playerCards.get(1).getRank().ordinal()) { //Which second card is higher? Maybe first Card is a Phoenix
--- src/server/model/Srv_HandTypeTest.java
+++ src/server/model/Srv_HandTypeTest.java
@@ -26,11 +26,38 @@ public class Srv_HandTypeTest { //@author Sandro, Thomas
             { "6S", "PE" },
     };
 
+    private static String[][] trippleCardsTable = {
+            { "5S", "5K", "5P" },
+    };
+
+    private static String[][] trippleCardsPlayer = {
+            { "6S", "6K", "6P" }, // case true regular
+            //{ "6S", "PE", "6P" }, //case true phoenix
+            //{ "6S", "6K", "4P" }, //case false
+    };
+
+    private static String[][] streetCardsTable = {
+            { "4S", "5S", "6K", "7P", "8P" },
+            //{ "4S", "5S", "6K", "7P", "8P", "9P" },
+    };
+
+    private static String[][] streetCardsPlayer = {
+          //  { "5K", "6K", "7P", "8P", "9P" }, //case true 5 cards
+           // { "TK", "JK", "QP", "KP", "AP" }, //case true 5 cards
+            { "9K", "TK", "JK", "QP", "KP", "AP" }, //case true 6 cards
+          //  { "5K", "6K", "7P", "8P", "9P", "TP" }, //case true 6 cards
+          //    { "6P", "7P", "8P", "9P" }, //case false
+    };
+
     // This is where we store the translated hands
     ArrayList<ArrayList<Srv_Card>> singleCardHandsTable;
     ArrayList<ArrayList<Srv_Card>> singleCardHandsPlayer;
     ArrayList<ArrayList<Srv_Card>> onePairHandsTable;
     ArrayList<ArrayList<Srv_Card>> onePairHandsPlayer;
+    ArrayList<ArrayList<Srv_Card>> trippleHandsTable;
+    ArrayList<ArrayList<Srv_Card>> trippleHandsPlayer;
+    ArrayList<ArrayList<Srv_Card>> streetHandsTable;
+    ArrayList<ArrayList<Srv_Card>> streetHandsPlayer;
 
     /**
      * The makeHands method is called before each test method,
@@ -43,6 +70,10 @@ public class Srv_HandTypeTest { //@author Sandro, Thomas
         singleCardHandsPlayer = makeHands(singleCardCardsPlayer);
         onePairHandsTable = makeHands(onePairCardsTable);
         onePairHandsPlayer = makeHands(onePairCardsPlayer);
+        trippleHandsTable = makeHands(trippleCardsTable);
+        trippleHandsPlayer = makeHands(trippleCardsPlayer);
+        streetHandsTable = makeHands(streetCardsTable);
+        streetHandsPlayer = makeHands(streetCardsPlayer);
     }
 
     @Test // This is the test method for isHigher in HandType.
@@ -83,6 +114,25 @@ public class Srv_HandTypeTest { //@author Sandro, Thomas
         }
     }
 
+    @Test // This is the test method for isHigher in HandType.
+    public void testisHigherTripple() {
+
+        ArrayList<Srv_Card> handTableZero = new ArrayList<Srv_Card>();
+        Srv_HandType ht = Srv_HandType.Tripple;
+
+        //Case OnePair Table has 0 Cards
+        for (ArrayList<Srv_Card> handPlayer : trippleHandsPlayer) {
+            assertTrue(Srv_HandType.Tripple.isHigher(handTableZero, handPlayer, ht));
+        }
+
+        //Case OnePair Table has 2 Cards
+        for (ArrayList<Srv_Card> handTable : trippleHandsTable) {
+            for (ArrayList<Srv_Card> handPlayer : trippleHandsPlayer) {
+                assertTrue(Srv_HandType.Tripple.isHigher(handTable, handPlayer, ht));
+            }
+        }
+    }
+
     @Test // This is the test method for isOnePair in HandType.
     public void testIsSingleCard() {
         for (ArrayList<Srv_Card> hand : singleCardHandsPlayer) {
@@ -94,6 +144,20 @@ public class Srv_HandTypeTest { //@author Sandro, Thomas
     public void testIsOnePair() {
         for (ArrayList<Srv_Card> hand : onePairHandsPlayer) {
                 assertTrue(Srv_HandType.isOnePair(hand));
+        }
+    }
+
+    @Test // This is the test method for isTripple in HandType.
+    public void testIsTripple() {
+        for (ArrayList<Srv_Card> hand : trippleHandsPlayer) {
+            assertTrue(Srv_HandType.isTripple(hand));
+        }
+    }
+
+    @Test // This is the test method for isStreet in HandType.
+    public void testIsStreet() {
+        for (ArrayList<Srv_Card> hand : streetHandsPlayer) {
+            assertTrue(Srv_HandType.isStreet(hand));
         }
     }
 
