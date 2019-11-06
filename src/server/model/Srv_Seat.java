80501bb026cee69ee724ba2ebe5d4f89d8571ba6
pascal.marbach@hotmail.com <(Breakdance1996)>
2019-11-06 17:15:59 +0100

    6.11.19 bookSeat--CompareTo

--- src/server/model/Srv_Seat.java
+++ src/server/model/Srv_Seat.java
@@ -3,35 +3,26 @@ package server.model;
 public class Srv_Seat {
 
     private final  int SEAT_ID ;
-    private Player player;
+    private Srv_Player player;
 
-    public Srv_Seat(int SEAT_ID){
-        this.SEAT_ID=SEAT_ID;
-        this.player=player;
-
-
-
-
+    public Srv_Seat(int SEAT_ID) {
+        this.SEAT_ID = SEAT_ID;
+        this.player = player;
 
     }
 
-
-
-
-
-    public Player getPlayer() {
-        return player;
-    }
 
     public int getSEAT_ID() {
         return SEAT_ID;
     }
 
-    public void setPlayer(Player player) {
-        this.player = player;
+
+    public Srv_Player getPlayer() {
+        return player;
     }
 
-
-
+    public void setPlayer(Srv_Player player) {
+        this.player = player;
+    }
 }
 
--- src/server/model/Srv_Team.java
+++ src/server/model/Srv_Team.java
@@ -1,14 +1,13 @@
 package server.model;
 
-import javax.swing.text.Style;
 import java.util.ArrayList;
 //@author Pascal
-public class Srv_Team {
+public  class Srv_Team implements Comparable<Srv_Team> {
 
     private final int TEAM_ID ;
-    private final int MAX_TEAM_MEMBERS=2;
+    private final int MAX_TEAM_MEMBERS;
     private ArrayList<Srv_Player>members;
-    private Srv_Round round;
+    private ArrayList<Srv_Seat>teamSeat;
     private Srv_Player player;
     private Srv_Seat seat;
     private Srv_Game game;
@@ -19,14 +18,18 @@ public class Srv_Team {
 
 
 
-
-    public Srv_Team(int TEAM_ID){
+        //Constructor
+    public Srv_Team(int TEAM_ID, Srv_Game game){
         this.TEAM_ID=TEAM_ID;
+        MAX_TEAM_MEMBERS =2;
         this.members=new ArrayList<Srv_Player>();
-        this.round=new Srv_Round();
-        this.player=new Player();
+        this.teamSeat=new ArrayList<Srv_Seat>();
+        this.player=player;
         this.gameScore=gameScore;
         this.roundScore=roundScore;
+        this.game=game;
+
+
 
 
 
@@ -35,34 +38,46 @@ public class Srv_Team {
     }
 
     private Srv_Seat bookSeat() {
-
+        while (this.teamSeat.size() < this.getMAX_TEAM_MEMBERS()) ;
+        this.teamSeat.add(this.seat);
+        this.members.add(seat.getPlayer());
         return seat;
 
     }
         //The GameScore is calculated depending on the rounds played.
         public void calcGameScore () {
-        for(int i=0;i<Srv_Round.getRounds().size();i++) {
-            this.gameScore +=(this.roundScore*=i);
+            for(int i=1;i<=this.game.getRounds().size();i++) {
+            this.gameScore +=this.roundScore;
+            i++;
         }
         }
+            //Compare the gameScore of the team
+            public int compareTo(Srv_Team t) {
+                return t.gameScore-this.gameScore;
+    }
 
 
-        // Resets all used variables and lists
+
+   /* // Resets all used variables and lists
         public void resetRoundScore () {
-        this.roundScore=0;
-        player.setSaidSmallTichu(false);
-        player.setSaidBigTichu(false);
-        round.getFinisher().clear();
-
-
+            this.roundScore=0;
         }
-
+*/
         public void calcRoundScore () {
+
+            //A team that is the first and second to loos all the cards gets 200 points.
+            Srv_Round lastRound = game.getRounds().get(game.getRounds().size()-1);//letzte runde zugewiesen
+            ArrayList<Player> finisher = lastRound.getFinisher();//finisher aus der letzt gespielten runde
+            if(finisher.get(0).getTeamId() == finisher.get(1).getTeamId() && this.TEAM_ID == finisher.get(0).getTeamId()){
+                roundScore = 200;
+            }
+
 
         /*If a small Tichu has been called, it is checked whether
             the player has finished the round first. Then he gets + 100 points, otherwise -100 points.*/
+
             if (player.isSaidSmallTichu() == true) {
-                if (player.getPLAYER_ID() == round.getFinischer().get(0)) {
+                if (player.getPLAYER_ID() == finisher.getFinischer().get(0)&&this.TEAM_ID==finisher.getTeamId()) {
                     this.roundScore += 100;
                 } else {
                     this.roundScore -= 100;
@@ -70,43 +85,31 @@ public class Srv_Team {
                 /*If a big Tichu has been called, it is checked whether
             the player has finished the round first. Then he gets + 100 points, otherwise -2 00 points.*/
                 if (player.isSaidBigTichu() == true) {
-                    if (player.getPLAYER_ID() == round.getFinischer().get(0)) {
+                    if (player.getPLAYER_ID() == finisher.getFinischer().get(0)&&this.TEAM_ID==finisher.getTeamId()) {
                         this.roundScore += 200;
                     } else {
                         this.roundScore -= 200;
 
 
                     }
-                }
-                /*Check the values of the cards in the array list wonCards.
-                 If the cards listed below occur, the value is assigned to the roundScore */
-                for (Card c : player.getWonCards()) {
-
-                    switch (Srv_Rank) {
-                        case Srv_Rank.king:
-                            this.roundScore += c.getValue();
-
-                        case Srv_Rank.Ten:
-                            this.roundScore += c.getValue();
-
-                        case Srv_Rank.Five:
-                            this.roundScore += c.getValue();
-
-                        case Srv_Rank.Dragon:
-                            this.roundScore += c.getValue();
-
-                        case Srv_Rank.Phoenix:
-                            this.roundScore += c.getValue();
-                    }
-                    //Doppelsieg fehlt noch
 
 
-                    }
+
+
 
 
                 }
 
-            }
+                    }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -142,4 +145,7 @@ public class Srv_Team {
     public void setRoundScore(int roundScore) {
         this.roundScore = roundScore;
     }
-}
+
+
+    }
+
