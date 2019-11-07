70eb81525e052fe28b46101696e4724dfc92f601
sandr094 <sandro.zimmermann@students.fhnw.ch>
2019-11-07 17:52:26 +0100

    Implementierung Countdown SZ

--- src/server/Tichu_Srv.java
+++ src/server/Tichu_Srv.java
@@ -1,6 +1,8 @@
 package server;
 
 import server.controller.Srv_Controller;
+import server.model.Srv_Countdown;
+import server.model.Srv_Deck;
 import server.model.Srv_Model;
 
 public class Tichu_Srv {
@@ -8,5 +10,11 @@ public class Tichu_Srv {
     public static void main(String[] args){
         Srv_Model model = new Srv_Model();
         Srv_Controller controller = new Srv_Controller(model);
+
+        Srv_Deck srv = new Srv_Deck(); //Testfall Deck
+        System.out.println(srv.getCards().size());
+
+        Srv_Countdown c1 = new Srv_Countdown(); //Testfall Countdown
+        c1.startCountdown();
     }
 }
---
+++ src/server/model/Srv_Countdown.java
@@ -0,0 +1,67 @@
+package server.model;
+
+import resources.ServiceLocator;
+import java.util.logging.Logger;
+
+public class Srv_Countdown extends Thread { //@author Sandro Countdown is a Thread
+
+    private static ServiceLocator sl = ServiceLocator.getServiceLocator();
+    private static Logger logger = sl.getLogger();
+    private int current = 0; //current value of the countdown
+    private int minimum = 0; //minimum value of the countdown
+    private int maximum = 30; //30 second time for a move
+    private boolean pause = true;
+
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000); //1 count = 1 second
+            } catch (Exception e) {
+                logger.info("Exception: "+e);
+            }
+
+            if (pause == false) { //countdown not paused? start to count
+                if (current <= maximum) {
+                    current++;
+                    logger.info("Counter: "+current);
+                }
+
+                if (current+1 > maximum) { //if maximum reached -> stopCountdown
+                    stopCountdown();
+                    logger.info("Countdown finished");
+                    break;
+                }
+            }
+        }
+    }
+
+    public void startCountdown() {
+        setCountdownPause(false);
+        super.start(); //start thread
+        logger.info("Start Countdown");
+    }
+
+    public void stopCountdown() {
+        setCountdownPause(true);
+        resetCountdown();
+        logger.info("Stop Countdown");
+    }
+
+    public void resetCountdown() {
+        current = minimum;
+        logger.info("Reset Countdown");
+    }
+
+    public boolean CountdownIsPause() {
+        return this.pause;
+    }
+
+    public void setCountdownPause(boolean pause) {
+        this.pause = pause;
+        if (this.pause == true) {
+            logger.info("Pause Countdown");
+        } else {
+            logger.info("UnPause Countdown");
+        }
+    }
+}
