d0047aaf4743cd6988c3171085a4098d62cdf373
sandr094 <sandro.zimmermann@students.fhnw.ch>
2019-11-17 17:19:06 +0100

    Anpassung HandTypeTest

--- src/server/model/Srv_Deck.java
+++ src/server/model/Srv_Deck.java
@@ -20,8 +20,8 @@ public class Srv_Deck { //@author Sandro
         for (Srv_Suit suit : Srv_Suit.values()) {
             for (Srv_Rank rank : Srv_Rank.values()) {
                 if (suit == Srv_Suit.SpecialCards && rank == Srv_Rank.Phoenix) { //create specialCards
-                    Srv_Card card = new Srv_Card(suit, rank, -25); //Phoenix as firstCard = 1.5
-                    card.setPhoenixRank(1.5);
+                    Srv_Card card = new Srv_Card(suit, rank, -25);
+                    card.setPhoenixRank(1.5); //Phoenix as firstCard = 1.5
                     this.cards.add(card);
                 }
                 if (suit == Srv_Suit.SpecialCards && rank == Srv_Rank.Dragon) {
